fce7a798e859c504bb4d41ead67ace5cd7233454
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-26 17:17:30 -0300

    moficacoes iniciais

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-    
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('DLzzz')
+})
+
+app.listen(port, () => {
+  console.log(`Server on na porta ${port}`)
+})
