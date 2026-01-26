cdab67cb68d820b0e05bbe06dccfe35aa461de0f
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-26 17:19:01 -0300

    modificacao no server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
-import express from 'express'
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
-const app = express()
-const port = 3000
+const app = express();
+const prisma = new PrismaClient();
+const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('DLzzz')
-})
+  res.send('DLzzz - API de Livros Online ');
+});
+
+app.get('/books', async (req, res) => {
+  const books = await prisma.book.findMany();
+  res.json(books);
+});
 
 app.listen(port, () => {
-  console.log(`Server on na porta ${port}`)
-})
+  console.log(` Server on na porta ${port}`);
+});
