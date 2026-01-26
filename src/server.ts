b027a0c691a499e8ba52dd39dba1c009c32e3aa4
Daniel Luiz <dnaieluiz1607@gmail.com>
2026-01-26 17:42:25 -0300

    modification in server

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,24 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('DLzzz - API de Livros Online ');
+// Cadastrar autores
+app.post('/authors', async (req, res) => {
+  const { name } = req.body;
+
+  try {
+    const newAuthor = await prisma.author.create({
+      data: { name },
+    });
+    return res.status(201).json(newAuthor);
+  } catch (error) {
+    return res.status(500).json({ error: "Erro ao criar autor." });
+  }
+});
+
+// Exibir Autores
+app.get('/authors', async (req, res) => {
+  const authors = await prisma.author.findMany();
+  return res.json(authors);
 });
 
 app.get('/books', async (req, res) => {
